59f9dbb95ba5a1a2e9eeb2e84c180306e73f984d
miamorko23 <christianprince.ranara@g.msuiit.edu.ph>
2023-12-02 13:27:22 +0800

    Ai Enhancement

--- portal.py
+++ portal.py
@@ -1,22 +1,24 @@
-# sea imports
-import psycopg2
-from scipy.spatial import distance
-from imutils import face_utils
-from pygame import mixer
-import imutils
-import dlib
-import cv2
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import requests
-import subprocess
-# 50f02ab2192c5a6e7e8ddcce9f82007f
 # https://rb.gy/d7pbc5
 import sys
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QCursor, QPixmap, QIcon
 from PyQt5.QtCore import Qt
+import psycopg2
+import subprocess
+
+# ~
+import csv
+from datetime import datetime
+import cv2
+import dlib
+import imutils
+import requests
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 
 def check_wifi_connection(ssid):
@@ -37,52 +39,100 @@ else:
     # sys.exit(0)
 
 
+# ~~
+# Record a drowsiness event locally
+def record_drowsiness_event_local(status):
+    with open('local_drowsiness_events.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([datetime.now().isoformat(), status])
+
+
+# Upload local drowsiness events to the database
+def upload_drowsiness_events_to_db(user_id):
+    import psycopg2
+    try:
+        conn = psycopg2.connect(
+            dbname="miamorko23",
+            user="miamorko23_user",
+            password="jsUL6Rbb6Ce1ZTopxZUtHO2hylVQIFA5",
+            host="dpg-ckn0mf91rp3c73epu630-a.singapore-postgres.render.com",
+            port="5432"
+        )
+
+        cur = conn.cursor()
+
+        # Read the local drowsiness events
+        with open('local_drowsiness_events.csv', mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                event_time, status = row
+                # Execute an INSERT SQL query
+                cur.execute("""
+                    INSERT INTO drowsinessevent (user_id, time, status)
+                    VALUES (%s, %s, %s)
+                """, (user_id, event_time, status))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print("Failed to upload drowsiness events:", e)
+
+
+# record_drowsiness_event_local('C')  # Record a critical event locally
+
+# After the drive is completed:
+# upload_drowsiness_events_to_db('06cf876a081c2963bd79ad7890213cb5')
+# ~~
+
+
+# Function to calculate the Eye Aspect Ratio (EAR)
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+# 06cf876a081c2963bd79ad7890213cb5
+
+# Main function for the drowsiness detection system
 def sea():
     try:
         # Set the ESP8266 IP address
         esp8266_ip = "192.168.4.1"
 
+        # Initialize the mixer and load the initial sound
+        mixer.init()
+        mixer.music.load("partial.mp3")
+
+        # Get the current volume and set it to a predefined level
         devices = AudioUtilities.GetSpeakers()
         interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
         volume = cast(interface, POINTER(IAudioEndpointVolume))
         current_volume_db = volume.GetMasterVolumeLevel()
-        if current_volume_db != -20.461252212524414:
-            volume.SetMasterVolumeLevel(-20.461252212524414, None)
+        volume.SetMasterVolumeLevel(-20.461252212524414, None)
 
-        # Define the coordinates for displaying the text on the screen
-        text_coordinates = (23, 300)
-        text_font = cv2.FONT_HERSHEY_SIMPLEX
-        text_scale = 0.7
-        text_color = (0, 255, 0)
-        text_thickness = 2
-
-        mixer.init()
-        mixer.music.load("partial.mp3")
-
-        def eye_aspect_ratio(eye):
-            a = distance.euclidean(eye[1], eye[5])
-            b = distance.euclidean(eye[2], eye[4])
-            c = distance.euclidean(eye[0], eye[3])
-            the_long_gun = (a + b) / (2.0 * c)
-            return the_long_gun
-
-        thresh = 0.25
-        frame_check = 10
+        # Initialize dlib's face detector and facial landmark predictor
         detect = dlib.get_frontal_face_detector()
         predict = dlib.shape_predictor("68FaceLandmarks.dat")
 
-        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+        # Grab the indexes of the facial landmarks for the left and right eye
+        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+        # Start the video stream
         cap = cv2.VideoCapture(0)
         flag = 0
-
-        leftEye = ""
-        rightEye = ""
-
         cnt = 0
         status = ''
         hp = 0
+
+        # Define the EAR threshold and the number of consecutive frames for checking
+        thresh = 0.25
+        frame_check = 10
 
         while True:
             ret, frame = cap.read()
@@ -90,31 +140,41 @@ def sea():
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             subjects = detect(gray, 0)
 
-            print('~!A')
-            print(len(subjects))
             for subject in subjects:
                 shape = predict(gray, subject)
                 shape = face_utils.shape_to_np(shape)
                 leftEye = shape[lStart:lEnd]
                 rightEye = shape[rStart:rEnd]
-
                 leftEAR = eye_aspect_ratio(leftEye)
                 rightEAR = eye_aspect_ratio(rightEye)
                 ear = (leftEAR + rightEAR) / 2.0
 
+                # Draw eye contours with a semi-transparent overlay
+                leftEyeHull = cv2.convexHull(leftEye)
+                rightEyeHull = cv2.convexHull(rightEye)
+                cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+                cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+                overlay = frame.copy()
+                cv2.fillPoly(overlay, [leftEyeHull], (0, 255, 0, 70))
+                cv2.fillPoly(overlay, [rightEyeHull], (0, 255, 0, 70))
+                cv2.addWeighted(overlay, 0.3, frame, 0.7, 0, frame)
+
+                # Connect the outer points of the eye contours to create sunglasses effect
+                leftmost_point = tuple(leftEye[leftEye[:, 0].argmin()])
+                rightmost_point = tuple(rightEye[rightEye[:, 0].argmax()])
+                cv2.line(frame, leftmost_point, rightmost_point, (0, 255, 0), 2)
+
                 if ear < thresh:
                     flag += 1
                     if flag >= frame_check:
-                        text_color = (0, 0, 255)
-                        cv2.putText(frame, "EYE CLOSED", text_coordinates, text_font, text_scale, text_color,
-                                    text_thickness)
-
+                        cv2.rectangle(frame, (10, 20), (250, 60), (0, 0, 255), -1)
+                        cv2.putText(frame, "EYE CLOSED", (10, 50),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
                         if cnt >= 3 and not mixer.music.get_busy():
                             mixer.music.load("final.mp3")
                             mixer.music.play()
                             status = 'Hercules'
-                            # This is where the Arduino Uno comes in
-                            requests.get(f"http://{esp8266_ip}/ledon")
+                            # requests.get(f"http://{esp8266_ip}/ledon")
                         if not mixer.music.get_busy():
                             mixer.music.play()
                             cnt += 1
@@ -138,34 +198,158 @@ def sea():
                             mixer.music.load("partial.mp3")
                             hp = 0
                             status = ''
-                            requests.get(f"http://{esp8266_ip}/ledoff")
+                            # requests.get(f"http://{esp8266_ip}/ledoff")
 
                     flag = 0
                     if sky == 'sky' or hi == 'hello':
                         cnt = 0
-                    cv2.putText(frame, "AWAKE", text_coordinates, text_font, text_scale, text_color, text_thickness)
+                    cv2.rectangle(frame, (10, 20), (200, 60), (0, 255, 0), -1)
+                    cv2.putText(frame, "AWAKE", (10, 50),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-            if cnt >= 3:
-                volume.SetMasterVolumeLevel(current_volume_db + 20.4, None)
+                if cnt >= 3:
+                    volume.SetMasterVolumeLevel(current_volume_db + 20.4, None)
 
-            # Draw squares around the eyes
-            left_eye_rect = cv2.boundingRect(leftEye)
-            right_eye_rect = cv2.boundingRect(rightEye)
-            cv2.rectangle(frame, left_eye_rect, text_color, text_thickness)
-            cv2.rectangle(frame, right_eye_rect, text_color, text_thickness)
-            text_color = (0, 255, 0)
-            cv2.imshow("DROWSINESS DETECTOR", frame)
+            cv2.imshow("Frame", frame)
             key = cv2.waitKey(1) & 0xFF
-
-            if key == ord("z"):
+            if key == ord('q'):
                 break
 
-        cv2.destroyAllWindows()
         cap.release()
+        cv2.destroyAllWindows()
+    except Exception as e:
+        print(e)
 
-    except Exception as pt:
-        print(pt)
-        sys.exit(0)
+
+# def sea():
+#     try:
+#         # Set the ESP8266 IP address
+#         esp8266_ip = "192.168.4.1"
+#
+#         devices = AudioUtilities.GetSpeakers()
+#         interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+#         volume = cast(interface, POINTER(IAudioEndpointVolume))
+#         current_volume_db = volume.GetMasterVolumeLevel()
+#         if current_volume_db != -20.461252212524414:
+#             volume.SetMasterVolumeLevel(-20.461252212524414, None)
+#
+#         # Define the coordinates for displaying the text on the screen
+#         text_coordinates = (23, 300)
+#         text_font = cv2.FONT_HERSHEY_SIMPLEX
+#         text_scale = 0.7
+#         text_color = (0, 255, 0)
+#         text_thickness = 2
+#
+#         mixer.init()
+#         mixer.music.load("partial.mp3")
+#
+#         def eye_aspect_ratio(eye):
+#             a = distance.euclidean(eye[1], eye[5])
+#             b = distance.euclidean(eye[2], eye[4])
+#             c = distance.euclidean(eye[0], eye[3])
+#             the_long_gun = (a + b) / (2.0 * c)
+#             return the_long_gun
+#
+#         thresh = 0.25
+#         frame_check = 10
+#         detect = dlib.get_frontal_face_detector()
+#         predict = dlib.shape_predictor("68FaceLandmarks.dat")
+#
+#         (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+#         (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+#
+#         cap = cv2.VideoCapture(0)
+#         flag = 0
+#
+#         leftEye = ""
+#         rightEye = ""
+#
+#         cnt = 0
+#         status = ''
+#         hp = 0
+#
+#         while True:
+#             ret, frame = cap.read()
+#             frame = imutils.resize(frame, width=523, height=323)
+#             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#             subjects = detect(gray, 0)
+#
+#             print('~!A')
+#             print(len(subjects))
+#             for subject in subjects:
+#                 shape = predict(gray, subject)
+#                 shape = face_utils.shape_to_np(shape)
+#                 leftEye = shape[lStart:lEnd]
+#                 rightEye = shape[rStart:rEnd]
+#
+#                 leftEAR = eye_aspect_ratio(leftEye)
+#                 rightEAR = eye_aspect_ratio(rightEye)
+#                 ear = (leftEAR + rightEAR) / 2.0
+#
+#                 if ear < thresh:
+#                     flag += 1
+#                     if flag >= frame_check:
+#                         text_color = (0, 0, 255)
+#                         cv2.putText(frame, "EYE CLOSED", text_coordinates, text_font, text_scale, text_color,
+#                                     text_thickness)
+#
+#                         if cnt >= 3 and not mixer.music.get_busy():
+#                             mixer.music.load("final.mp3")
+#                             mixer.music.play()
+#                             status = 'Hercules'
+#                             # This is where the Arduino Uno comes in
+#                             requests.get(f"http://{esp8266_ip}/ledon")
+#                         if not mixer.music.get_busy():
+#                             mixer.music.play()
+#                             cnt += 1
+#                             if status == 'Hercules':
+#                                 hp = 0
+#
+#                 else:
+#                     hi = 'hi'
+#                     sky = 'sky'
+#                     if cnt >= 2 and status != 'Hercules':
+#                         sky = 'blue'
+#                         if not mixer.music.get_busy():
+#                             volume.SetMasterVolumeLevel(current_volume_db, None)
+#                             hi = 'hello'
+#                     if status == 'Hercules':
+#                         if not mixer.music.get_busy():
+#                             mixer.music.play()
+#                             hp += 1
+#                         elif hp == 1:
+#                             volume.SetMasterVolumeLevel(current_volume_db, None)
+#                             mixer.music.load("partial.mp3")
+#                             hp = 0
+#                             status = ''
+#                             requests.get(f"http://{esp8266_ip}/ledoff")
+#
+#                     flag = 0
+#                     if sky == 'sky' or hi == 'hello':
+#                         cnt = 0
+#                     cv2.putText(frame, "AWAKE", text_coordinates, text_font, text_scale, text_color, text_thickness)
+#
+#             if cnt >= 3:
+#                 volume.SetMasterVolumeLevel(current_volume_db + 20.4, None)
+#
+#             # Draw squares around the eyes
+#             left_eye_rect = cv2.boundingRect(leftEye)
+#             right_eye_rect = cv2.boundingRect(rightEye)
+#             cv2.rectangle(frame, left_eye_rect, text_color, text_thickness)
+#             cv2.rectangle(frame, right_eye_rect, text_color, text_thickness)
+#             text_color = (0, 255, 0)
+#             cv2.imshow("DROWSINESS DETECTOR", frame)
+#             key = cv2.waitKey(1) & 0xFF
+#
+#             if key == ord("z"):
+#                 break
+#
+#         cv2.destroyAllWindows()
+#         cap.release()
+#
+#     except Exception as pt:
+#         print(pt)
+#         sys.exit(0)
 
 
 class LoginWindow(QtWidgets.QWidget):
